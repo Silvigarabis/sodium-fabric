960dd02ce7a245d4c2384bcc34743d9345b165d4
7777777_4547 <83630775+7777777-4547@users.noreply.github.com>
2022-06-19 14:23:59 +0800

    I stupid

--- src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumOptionsGUI.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumOptionsGUI.java
@@ -88,7 +88,7 @@ public class SodiumOptionsGUI extends Screen {
         this.applyButton = new FlatButtonWidget(new Dim2i(this.width - 142, this.height - 26, 65, 20), new TranslatableText("Apply").getString(), this::applyChanges);
         this.closeButton = new FlatButtonWidget(new Dim2i(this.width - 73, this.height - 26, 65, 20), new TranslatableText("Close").getString(), this::onClose);
         this.donateButton = new FlatButtonWidget(new Dim2i(this.width - 128, 6, 100, 20), new TranslatableText("Buy us a coffee!").getString(), this::openDonationPage);
-        this.hideDonateButton = new FlatButtonWidget(new Dim2i(this.width - 26, 6, 20, 20), ("x"), this::hideDonationButton);
+        this.hideDonateButton = new FlatButtonWidget(new Dim2i(this.width - 26, 6, 20, 20), "x", this::hideDonationButton);
 
         if (SodiumClientMod.options().notifications.hideDonationButton) {
             this.setDonationButtonVisibility(false);
