9e7079f9759bf65a519d7aed249e1773a3511f4b
7777777_4547 <83630775+7777777-4547@users.noreply.github.com>
2022-08-01 16:17:59 +0800

    fix, update zh_cn.json

--- src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptionPages.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptionPages.java
@@ -34,7 +34,7 @@ public class SodiumGameOptionPages {
                 .add(OptionImpl.createBuilder(int.class, vanillaOpts)
                         .setName(new TranslatableText("options.renderDistance").getString())
                         .setTooltip(new TranslatableText("sodium.options.view_distance.tooltip").getString())
-                        .setControl(option -> new SliderControl(option, 2, 32, 1, ControlValueFormatter.quantity("Chunks")))
+                        .setControl(option -> new SliderControl(option, 2, 32, 1, ControlValueFormatter.quantity(new TranslatableText("options.chunks").getString())))
                         .setBinding((options, value) -> options.viewDistance = value, options -> options.viewDistance)
                         .setImpact(OptionImpact.HIGH)
                         .setFlags(OptionFlag.REQUIRES_RENDERER_RELOAD)
